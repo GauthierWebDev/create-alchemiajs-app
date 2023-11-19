11428320ded0a555b9cfeb061eb5c8673204d927
GauthierWebDev <gauthier@gauthierdaniels.fr>
2023-11-19 22:48:40 +0100

    feat: Add minify middleware to compress HTML responses

--- template/src/server.ts
+++ template/src/server.ts
@@ -1,5 +1,10 @@
+import * as middlewares from "@/middlewares";
 import express from "express";
 
 const server = express();
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(middlewares.minify);
 
 export default server;
